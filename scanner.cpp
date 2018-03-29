94cee0279612780e1e1c8a66225b833951e69359
lelthar <geraldm1998@hotmail.com>
2018-03-28 18:01:32 -0600

    se agrego el buffer de 1kb

---
+++ scanner.cpp
@@ -0,0 +1,36 @@
+
+#include <stdio.h>
+#include <string.h>
+#include <iostream>     // std::cout
+#include <fstream>      // std::ifstream
+#include "scanner.h"
+
+int main()
+{
+    char archivo[] = {"foo.txt"};
+	leer_archivo(&archivo[0]);
+    return 0;
+}
+
+void leer_archivo(char *archivo_nombre){
+	std::ifstream archivo(archivo_nombre,  std::ios_base::in);
+    if (archivo.is_open())
+    {
+        char* buffer = new char[1024];
+        while (archivo.read(buffer, 1024))
+        {
+            std::string linea_buffer(buffer);
+            std::cout<< linea_buffer << std::endl;
+        }
+
+       std::string linea_buffer(buffer, archivo.gcount()); //agrega al string el resto de caracteres que quedan en el buffer
+       std::cout<< linea_buffer << std::endl;
+
+       delete[] buffer;
+       archivo.close();
+   }
+   else
+   {
+        std::cout<< "No se puso abrir el archivo:" << archivo_nombre << std::endl;
+   }
+}
